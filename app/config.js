5487ba466419e2300556eaeae4da7a1e66a9b77a
Chris Sisk <chris@sisk.co>
2018-09-24 10:30:58 +0100

    content update

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'HMCTS API Portal',
+  serviceName: 'HMCTS API portal',
 
   // Default port that prototype runs on
   port: '3000',
